c14ea9a2897f98c2cd13fdd438cec6b48a82f763
Vladimir Miroshnichenko <i@gururuby.ru>
2016-07-11 20:56:32 +0300

    work

--- functions.js
+++ functions.js
@@ -78,13 +78,12 @@ testBtn.on('click', toggle);
  */
 
 
-var answersArray = [
+var answers = [
     "\"10\"", -1, 1, 2, 6, "\"9px\"", "\"$45\"", 2, NaN, "\" -9\n5\"",
     -14, 2, 5, 5, 5, 1, "NaN", "false", "true"
 ];
 
-
-
+var rightAnswers = [];
 
 function checkTest() {
 
@@ -92,22 +91,26 @@ function checkTest() {
     var right = "<span class=\"glyphicon glyphicon-ok form-control-feedback\"></span>";
     var wrong = "<span class=\"glyphicon glyphicon-remove form-control-feedback\"></span>";
     var testInputs = $('#test-questions').find('input');
-    var numberOfRightAnswers = 0;
+
 
     function isRight(element) {
-        return element.val() == answersArray[element.data('question') - 1];
+        var answerNumber = element.data('answer') - 1;
+        var result = element.val();
+        if (result == answers[answerNumber]) {
+            rightAnswers[answerNumber] = true;
+        }
+        else {
+            rightAnswers[answerNumber] = false;
+        }
+        return rightAnswers[answerNumber];
     }
-
 
     testInputs.on('change', function () {                                 //Когда меняется конкретная форма:
 
         var $input = $(this),
             $parent = $(this).parent();
 
-
-        if ($input.siblings().is('span') == true) {
-            $input.siblings().remove();
-            $parent.removeClass('has-success has-error')}
+        clearInput(this);
 
         if (isRight($(this))) {                                          //Если введен правильный ответ:
 
@@ -119,12 +122,11 @@ function checkTest() {
 
             if ($parent.is('has-success') == false) {                    //Если у формы нет класса success, то добавляем его
                 $parent.addClass("has-success has-feedback");
-                numberOfRightAnswers++;
             }
 
         } else {                                                        //Если введен неправильный ответ:
 
-            $input.after(wrong); //Добавляем значок wrong
+            $input.after(wrong);                                         //Добавляем значок wrong
 
             if ($parent.is('has-success') == true) {                       //Если у формы класс success, удаляем его
                 $parent.removeClass('has-success has-feedback');
@@ -132,19 +134,38 @@ function checkTest() {
 
             if ($parent.is('has-error') == false) {                    //Если у формы нет класса error, то добавляем его
                 $parent.addClass("has-error has-feedback");
-                numberOfRightAnswers--;
             }
         }
+        var rightAnswersCount = rightAnswers.filter(function (x) {
+            return x == true
+        }).length;
 
-        if (numberOfRightAnswers < 0) {numberOfRightAnswers = 0};
-        $('#count').html(numberOfRightAnswers);
+        $('#count').html(rightAnswersCount);
     });
 }
 
-function clearTestBtn() {
-    $('#test-questions').find('input').empty();
+function clearTest() {
+    var allInputs = $('#test-questions').find('input');
+    allInputs.val(undefined);
+    clearInputs(allInputs);
+}
+
+function clearInput(input) {
+    var $input = $(input);
+    if ($input.siblings().is('span') == true) {
+        $input.siblings().remove();
+        $input.parent().removeClass('has-success has-error');
+    }
+}
+
+function clearInputs(inputs) {
+    inputs.each(function(index, element){
+        $(element).trigger('change');
+        clearInput(element);
+    });
 }
 
 
+$('#clear-button').on('click', clearTest);
 
 checkTest();
