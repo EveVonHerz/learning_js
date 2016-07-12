b5761bb218256f170e7839c79f1afe6d6b660368
eve <eve.von.herz@gmail.com>
2016-07-12 19:57:43 +0300

    work by Vladimir Miroshnichenko

--- js_skills_testing.js
+++ js_skills_testing.js
@@ -1,72 +1,71 @@
-var jsSkillsTesting = {
+var App = {
 
-    answers: [
-        "\"10\"", -1, 1, 2, 6, "\"9px\"", "\"$45\"", 2, NaN, "\" -9\n5\"",
-        -14, 2, 5, 5, 5, 1, "NaN", "false", "true"
-    ],
+    // answers: [
+    //     "\"10\"", -1, 1, 2, 6, "\"9px\"", "\"$45\"", 2, NaN, "\" -9\n5\"",
+    //     -14, 2, 5, 5, 5, 1, "NaN", "false", "true"
+    // ],
+    answers: {
+        1: ['10'],
+        2: [-1],
+        3: [1, '1'],
+        4: [2],
+        5: [6],
+        6: ['"9px"', "'9px'"],
+        7: ["\"$45\""],
+        8: [2],
+        9: [NaN],
+        10: ["\" -9\n5\""],
+        11: [-14],
+        12: [2],
+        13: [5],
+        14: [5],
+        15: [5],
+        16: [1],
+        17: ["NaN"],
+        18: ["false"],
+        19: ["true"]
+    },
     rightAnswers: [],
     rightSpanTemplate: "<span class=\"glyphicon glyphicon-ok form-control-feedback\"></span>",
     wrongSpanTemplate: "<span class=\"glyphicon glyphicon-remove form-control-feedback\"></span>",
     testInputs: $('#test-questions').find('input'),
 
-    run: function () {
-        $('#clear-button').on('click', jsSkillsTesting.clearTest);
+    init: function (){
+      App.runTest();
+    },
 
-        jsSkillsTesting.testInputs.on('change', function () {                                 //Когда меняется конкретная форма:
-            var $input = $(this),
-                $parent = $(this).parent();
+    runTest: function () {
+        App.clearTest();
 
-            jsSkillsTesting.clearInput(this);
-
-            if (jsSkillsTesting.isRightAnswer($(this))) {                                          //Если введен правильный ответ:
-
-                $input.after(jsSkillsTesting.rightSpanTemplate);
-
-                if ($parent.is('has-error') == true) {                       //Если у формы класс error, удаляем его
-                    $parent.removeClass('has-error has-feedback');
-                }
-
-                if ($parent.is('has-success') == false) {                    //Если у формы нет класса success, то добавляем его
-                    $parent.addClass("has-success has-feedback");
-                }
-
-            } else {                                                        //Если введен неправильный ответ:
-
-                $input.after(jsSkillsTesting.wrongSpanTemplate);                                         //Добавляем значок wrong
-
-                if ($parent.is('has-success') == true) {                       //Если у формы класс success, удаляем его
-                    $parent.removeClass('has-success has-feedback');
-                }
-
-                if ($parent.is('has-error') == false) {                    //Если у формы нет класса error, то добавляем его
-                    $parent.addClass("has-error has-feedback");
-                }
+        App.testInputs.on('change', function () {
+            App.clearInput(this);
+            if (App.isRightAnswer($(this))) {
+                App.applyRight($(this));
+            } else {
+                App.applyWrong($(this));
             }
-            var rightAnswersCount = jsSkillsTesting.rightAnswers.filter(function (x) {
-                return x == true
-            }).length;
-
-            $('#count').html(rightAnswersCount);
-
+            App.updateRightAnswerCounter();
         });
     },
+
     isRightAnswer: function (answer) {
-        var answerNumber = answer.data('answer') - 1;
-        var result = answer.val();
-        if (result == jsSkillsTesting.answers[answerNumber]) {
-            jsSkillsTesting.rightAnswers[answerNumber] = true;
+        var answerNumber = answer.data('answer');
+        var result = $.trim(answer.val());
+        if (App.answers[answerNumber].indexOf(result) != -1) {
+            App.rightAnswers[answerNumber] = true;
         }
         else {
-            jsSkillsTesting.rightAnswers[answerNumber] = false;
+            App.rightAnswers[answerNumber] = false;
         }
-        return jsSkillsTesting.rightAnswers[answerNumber];
+        return App.rightAnswers[answerNumber];
     },
 
-
     clearTest: function () {
-        var allInputs = $('#test-questions').find('input');
-        allInputs.val(undefined);
-        jsSkillsTesting.clearInputs(allInputs);
+        $('#clear-button').on('click', function(){
+            var allInputs = $('#test-questions').find('input');
+            allInputs.val(undefined);
+            App.clearInputs(allInputs);
+        });
     },
 
     clearInput: function (input) {
@@ -80,10 +79,39 @@ var jsSkillsTesting = {
     clearInputs: function (inputs) {
         inputs.each(function (index, element) {
             $(element).trigger('change');
-            jsSkillsTesting.clearInput(element);
+            App.clearInput(element);
         });
-    }
+    },
 
+    applyRight: function (currentInput) {
+        var $parent = currentInput.parent();
+
+        currentInput.after(App.rightSpanTemplate);
+        if ($parent.is('has-error')) {
+            $parent.removeClass('has-error has-feedback');
+        }
+        if (!$parent.is('has-success')) {
+            $parent.addClass("has-success has-feedback");
+        }
+    },
+
+    applyWrong: function (currentInput) {
+        var $parent = currentInput.parent();
+        currentInput.after(App.wrongSpanTemplate);
+        if ($parent.is('has-success')) {
+            $parent.removeClass('has-success has-feedback');
+        }
+        if (!$parent.is('has-error')) {
+            $parent.addClass("has-error has-feedback");
+        }
+    },
+
+    updateRightAnswerCounter: function () {
+        var rightAnswersCount = App.rightAnswers.filter(function (x) {
+            return x == true
+        }).length;
+        $('#count').html(rightAnswersCount);
+    }
 };
 
-jsSkillsTesting.run();
+App.init();
