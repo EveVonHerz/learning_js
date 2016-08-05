46be7d88ce9515f75526538ee15446cee5c31b4e
EveVonHerz <eve.von.herz@gmail.com>
2016-08-05 17:14:08 +0300

    work

--- js/my/while.js
+++ js/my/while.js
@@ -10,71 +10,29 @@ function evenNumbers() {
 
     // Сделать проверку на буквы
 
-    var $inputLastEvenNumber = $('#last-even');
-    var $inputEvenNumbers = $('#even-numbers');
+    var $inputLastNumber = $('#last-even');
+    var $lastNumber = $('#last-even').val();
 
+    var $inputResult = $('#even-numbers');
 
-    $inputLastEvenNumber.on('change', arrayOfNumbersWithCondition($inputLastEvenNumber, $inputEvenNumbers, 'even'));
+    $inputLastNumber.on('change', function() {even($lastNumber, $inputResult)});
 
-    //
-    // $inputLastEvenNumber.on('change', function () {
-    //     var evenNumbers = [];
-    //     $inputEvenNumbers.val('');
-    //
-    //     var $lastEvenNumber = $(this).val();
-    //     if ($lastEvenNumber < 2) {
-    //         $inputEvenNumbers.val('Вы ввели число меньше 2')
-    //     } else {
-    //         for (var evenNumber = 2; evenNumber <= $lastEvenNumber; evenNumber++) {
-    //             if (evenNumber % 2 == 0) {
-    //                 evenNumbers.push(' ' + evenNumber)
-    //             }
-    //         }
-    //         $inputEvenNumbers.val(evenNumbers);
-    //     }
-    // })
+    function even(lastNumber, inputResult) {
+        var array = [];
 
-
-
-}
-
-function arrayOfNumbersWithCondition(input, result, condition) {
-    var array = [];
-    input.val('');
-
-    var lastNumber = $(input).val();
-    if (lastNumber < 2) {
-        result.val('Вы ввели число меньше 2')
-    } else {
-
-        if (condition == 'even') {
-            for (var num = 2; num <= lastNumber; num++) {
-                if (num % 2 == 0) {
-                    array.push(' ' + num)
+        if (lastNumber < 2) {
+            inputResult.val(lastNumber)
+        } else {
+            for (var number = 2; number <= lastNumber; number++) {
+                if (number % 2 == 0) {
+                    array.push(' ' + number)
                 }
             }
+            inputResult.val(array);
         }
-
-        if (condition == 'simple') {
-            for (var num = 2; num <= lastNumber-1; num++) {
-                if (num % 2 != 0) {
-                    array.push(' ' + num)
-                }
-            }
-        }
-
-        result.val(array);
     }
-}
 
-// function simpleNumbers() {
-//
-//     var $inputLastSimpleNumber = $('#last-simple');
-//     var $inputSimpleEvenNumbers = $('#simple-numbers');
-//
-//     $inputLastSimpleNumber,on('change', function)
-//
-// }
+}
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
